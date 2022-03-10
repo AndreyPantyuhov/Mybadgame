e27f3c51b3c5df442ede1808135f48034cb1bcd6
AndreyPantyuhov <100298649+AndreyPantyuhov@users.noreply.github.com>
2022-03-10 19:11:32 +0500

    Update mybadgame.py

--- mybadgame.py
+++ mybadgame.py
@@ -9,6 +9,12 @@ HEIGHT_DISPLAY = 480
 WIDTH_PLAYER = 40
 HEIGHT_PLAYER = 40
 
+# Переменные отвечающие за передвижение игрока: r - right, l - left, u - up, d - down
+r = False
+l = False
+u = False
+d = False
+
 # Константы отвечающее за цвета
 RED = 255, 0, 0
 GREEN = 0, 255, 0
@@ -18,6 +24,7 @@ BLACK = 0, 0, 0
 
 # Создание класса игрока
 class Player(pygame.sprite.Sprite):
+  # Инициализация игрока
   def __init__(self):
     pygame.sprite.Sprite.__init__(self)
     self.image = pygame.Surface((WIDTH_PLAYER, HEIGHT_PLAYER))
@@ -25,10 +32,25 @@ class Player(pygame.sprite.Sprite):
     self.rect = self.image.get_rect()
     self.rect.center = ((WIDTH_DISPLAY / 2, HEIGHT_DISPLAY / 2))
  
+ # Обновление позиции игрока 
  def update(self):
-   self.rect.x += 1
+   if r == True:
+    self.rect.x += 1
+   if l == True:
+    self.rect.y -= 1
+   if u == True:
+    self.rect.y -= 1
+   if d == True:
+    self.rect.y += 1
+    
    if self.rect.x > WIDTH_DISPLAY:
      self.rect.x = 0
+   if self.rect.x < 0:
+     self.rect.x = WIDTH_DISPLAY
+   if self.rect.top < HEIGHT_DISPLAY:
+     self.rect.y = 0
+   if self.rect.bottom < 0:
+     self.rect.y = HEIGHT_DISPLAY
 
 # Инициализация дисплея и спрайтов
 pygame.init()
